23f2c46b5d92a9eadab765807a67c81454093013
K1Dhanush <abhireddy11304@gmail.com>
2024-02-12 18:30:11 +0530

    Initial Commit

--- src/main/java/com/db/pratice/security/SecurityConfig.java
+++ src/main/java/com/db/pratice/security/SecurityConfig.java
@@ -4,6 +4,7 @@ package com.db.pratice.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,39 +24,38 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    /*
+    @Bean
+    public UserDetailsService userDetails(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}123456789")
+                .roles("admin")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+    */
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/update/**").hasRole("admin")
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/addUser/**").hasRole("admin")
+                        .requestMatchers(HttpMethod.GET).permitAll()
                         //.hasRole("admin")
                         .anyRequest()
                 .authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
-    public InMemoryUserDetailsManager userDetails(){
-        UserDetails admin = User.withUsername("admin")
-                .password("{noop}123456789")
-                .roles("admin")
-                .build();
-        return new InMemoryUserDetailsManager(admin);
-    }
+
     @Autowired
     private CustomerUserDetailsService userDetailsService;
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-    }
+/*
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+*/
 }
